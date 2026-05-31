5500b868e8ff3a2c4f4e9281899b56f2c29b31ba
jurmanudesa <juanurman@gmail.com>
2026-05-31 13:10:38 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: './', // Permite que los archivos se carguen usando rutas relativas en GitHub Pages
+  base: '/Reporte_Financiero/', // Usamos el nombre exacto de tu repositorio
 });
